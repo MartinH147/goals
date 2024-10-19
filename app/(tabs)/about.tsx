10293c72708437bdee905b50ff4ab8e081bfe403
MartinH147 <martinh13@optusnet.com.au>
2024-10-19 17:53:02 +1100

    didn't do much

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,11 +1,17 @@
 import { Text, View, StyleSheet } from "react-native";
+import { Card } from "react-native-elements";
 import { LinearGradient } from "expo-linear-gradient";
 
 export default function AboutScreen() {
   return (
     <View style={styles.container}>
       <LinearGradient colors={["#f9b16e", "#f68080"]} style={styles.gradient}>
-        <Text style={styles.text}>Goals screen</Text>
+        <Card title="HELLO WORLD">
+          <Text style={{ marginBottom: 10 }}>
+            The idea with React Native Elements is more about component
+            structure than actual design.
+          </Text>
+        </Card>
       </LinearGradient>
     </View>
   );
